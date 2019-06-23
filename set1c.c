8c267727452347ddc6d19e8d26ebb9b869498e45
muthu-gokul <36202759+muthu-gokul@users.noreply.github.com>
2019-06-23 10:13:28 +0530

    Create set1c.c

---
+++ set1c.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int ascii;
+char alpha;
+scanf("%c",&c);
+ascii=alpha;
+if(ascii>96&&ascii<123)
+{
+if(ascii==97||ascii==101||ascii==105||ascii==111||ascii==117)
+{
+    printf("Vowel");
+}
+else
+{
+    printf("Consonant");
+}}
+else
+{
+        printf("invalid");
+}
+}
